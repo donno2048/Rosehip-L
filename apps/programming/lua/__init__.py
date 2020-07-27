094cda4d4185a1750c2d2370e91ea7d8580bcdd4
donno2048 <61805754+donno2048@users.noreply.github.com>
2020-07-27 22:38:08 +0300

    Create __init__.py

---
+++ apps/programming/lua/__init__.py
@@ -0,0 +1,11 @@
+import os;import pygame;pygame.init();font = pygame.font.Font(None, 32);clock = pygame.time.Clock();input_box = pygame.Rect(100, 100, 140, 32);out_box=pygame.Rect(100,200, 140, 32);from pygame_gui.elements import UIWindow;from pygame_gui.elements import UITextBox;from pygame_gui.elements import UITextEntryLine;from pygame_gui.elements import UITextBox;import pygame_gui
+class lua(UIWindow):
+    def __init__(self, pos, manager):super().__init__(pygame.Rect(pos, (400, 300)), manager, window_display_title="lua", object_id="#lua",resizable=True);self.textbox = pygame_gui.elements.UITextBox("",relative_rect=pygame.Rect(0, 0, 368, 200),manager=manager,container=self,anchors={"left": "left","right": "right","top": "top","bottom": "bottom",},);self.input = pygame_gui.elements.UITextEntryLine(relative_rect=pygame.Rect(0, -35, 368, 30),manager=manager,container=self,anchors={"left": "left","right": "right","top": "bottom","bottom": "bottom",},);self.text='';self.manager=manager;self.input.focus()
+    def process_event(self, event):
+        super().process_event(event)
+        if event.type == pygame.KEYUP and event.key == pygame.K_RETURN:os.chdir(os.path.dirname(os.path.abspath(__file__)));open('pylu','w').writelines(self.input.get_text().split('|'));self.text+='<br>'+os.popen('lua pylu').read().replace('\n','<br>');os.remove('pylu');self.input.kill();self.textbox.kill();self.textbox = pygame_gui.elements.UITextBox(self.text,relative_rect=pygame.Rect(0, 0, 368, 200),manager=self.manager,container=self,anchors={"left": "left","right": "right","top": "top","bottom": "bottom",},);self.input = pygame_gui.elements.UITextEntryLine(relative_rect=pygame.Rect(0, -35, 368, 30),manager=self.manager,container=self,anchors={"left": "left","right": "right","top": "bottom","bottom": "bottom",},);self.input.focus()
+def load(manager, params):
+    if 'Installed-Size:' not in os.popen('dpkg -s lua5.3').read():os.system('sudo apt install lua5.3')
+    pos = (100, 100)
+    if params is not None and len(params) > 0:pos = params[0]
+    lua(pos, manager)
