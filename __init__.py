2d4fe80b0a0c9c8800049cc67f2dfc4a6fd9bf5b
Elisha Hollander <just4now666666@gmail.com>
2020-08-23 14:20:48 +0300

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,1 +1,2 @@
-#this is here for people who wants to import this module
+from Rosehip.os import pc;from os import system
+def start():system('sudo apt get update && sudo apt install python3-pip && pip3 install requests opencv-python selenium');pc()
