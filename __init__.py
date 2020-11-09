4e8a3dd79a446f293b225ae84d8ec12b202f0b3a
Elisha Hollander <just4now666666@gmail.com>
2020-11-09 22:36:04 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,2 +1,2 @@
 from Rosehip.os import pc;from os import system
-def start():system('sudo apt get update && sudo apt install python3-pip && pip3 install requests opencv-python selenium markdown2');pc()
+def start():pc()
