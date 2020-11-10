082fc84ab9196578bc6657369a756602e82fbf3a
Elisha Hollander <just4now666666@gmail.com>
2020-11-10 09:19:14 +0200

    Update __init__.py

--- apps/programming/c++/__init__.py
+++ apps/programming/c++/__init__.py
@@ -5,7 +5,7 @@ class Cpp(UIWindow):
         super().process_event(event)
         if event.type == pygame.KEYUP and event.key == pygame.K_RETURN:os.chdir(os.path.dirname(os.path.abspath(__file__)));open('py.cc','w').writelines(self.input.get_text().split('|'));os.system('g++ -o py+ py.cc');self.text+='<br>'+os.popen('./py+').read().replace('\n','<br>');os.remove('py.cc');os.remove('py+');self.input.kill();self.textbox.kill();self.textbox = pygame_gui.elements.UITextBox(self.text,relative_rect=pygame.Rect(0, 0, 368, 200),manager=self.manager,container=self,anchors={"left": "left","right": "right","top": "top","bottom": "bottom",},);self.input = pygame_gui.elements.UITextEntryLine(relative_rect=pygame.Rect(0, -35, 368, 30),manager=self.manager,container=self,anchors={"left": "left","right": "right","top": "bottom","bottom": "bottom",},);self.input.focus()
 def load(manager, params):
-    if 'Installed-Size:' not in os.popen('dpkg -s g++').read():os.system('sudo apt install g++')
+    if 'Installed-Size:' not in os.popen('dpkg -s g++').read():os.popen('sudo apt install g++')
     pos = (100, 100)
     if params is not None and len(params) > 0:pos = params[0]
     Cpp(pos, manager)
