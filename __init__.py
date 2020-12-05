ea8acaa579a3183627a52390920a0e508611006b
Elisha Hollander <just4now666666@gmail.com>
2020-12-05 20:17:03 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,2 +1,2 @@
-from Rosehip.os import pc;from os import popen
-def start():popen('sudo apt-get update && sudo apt install python3-pip && pip3 install requests opencv-python selenium');pc()
+from Rosehip.os import OS;from os import popen
+def start():popen('sudo apt-get update && sudo apt install python3-pip && pip3 install requests opencv-python selenium');OS().run()
